5e0467e0c3521e7355fd527a8fff88eb01ebce73
dark_flower <anosv3380@gmail.com>
2025-05-26 14:56:36 -0700

    Removed comments.

--- Forge-Tutorial-1.20.X-1-setup/src/main/java/net/jinx/netherspace/NetherSpace.java
+++ Forge-Tutorial-1.20.X-1-setup/src/main/java/net/jinx/netherspace/NetherSpace.java
@@ -13,7 +13,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(NetherSpace.MOD_ID)
 public class NetherSpace {
     public static final String MOD_ID = "netherspace";
@@ -32,18 +31,15 @@ public class NetherSpace {
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
