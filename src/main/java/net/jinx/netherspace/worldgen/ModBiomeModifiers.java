2d36190308bda3c4b952cf365641c03f8a36d845
dark_flower <anosv3380@gmail.com>
2025-06-06 20:25:09 -0700

    added tree gen

--- src/main/java/net/jinx/netherspace/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/jinx/netherspace/worldgen/ModBiomeModifiers.java
@@ -8,6 +8,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,6 +16,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_NETHER_NETHER_ORE = registerKey("add_nether_nether_ore");
     public static final ResourceKey<BiomeModifier> ADD_END_NETHER_ORE = registerKey("add_end_nether_ore");
+
+    public static final ResourceKey<BiomeModifier> ADD_TREE_NETHER = registerKey("add_tree_nether");
 
 
     public static void bootstrap(BootstapContext<BiomeModifier> context) {
@@ -30,6 +33,11 @@ public class ModBiomeModifiers {
                 biomes.getOrThrow(BiomeTags.IS_END),
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.END_NETHER_ORE_PLACED_KEY)),
                 GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_TREE_NETHER, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(Tags.Biomes.IS_COLD_END),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.NETHER_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION));
     }
 
 
--- src/main/java/net/jinx/netherspace/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/jinx/netherspace/worldgen/ModPlacedFeatures.java
@@ -1,10 +1,13 @@
 package net.jinx.netherspace.worldgen;
 
 import net.jinx.netherspace.NetherSpace;
+import net.jinx.netherspace.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -19,6 +22,8 @@ public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> NETHER_NETHER_ORE_PLACED_KEY = registerKey("nether_nether_ore_placed");
     public static final ResourceKey<PlacedFeature> END_NETHER_ORE_PLACED_KEY = registerKey("end_nether_ore_placed");
 
+    public static final ResourceKey<PlacedFeature> NETHER_PLACED_KEY = registerKey("nether_placed");
+
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
@@ -29,6 +34,10 @@ public class ModPlacedFeatures {
         register(context, END_NETHER_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_NETHER_ORE_KEY),
                 ModOrePlacement.rareOrePlacement(3,
                         HeightRangePlacement.triangle(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
+
+        register(context, NETHER_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.NETHER_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(1, 0.1f, 1),
+                        ModBlocks.NETHER_SAPLING.get()));
     }
 
 
